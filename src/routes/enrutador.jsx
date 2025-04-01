cc99758cffa78846ec06a8855819af236d690609
jaimezzapata <zapataval2304@gmail.com>
2025-03-31 19:14:40 -0500

    Se crea enrutador básico de la aplicación

---
+++ src/routes/enrutador.jsx
@@ -0,0 +1,21 @@
+import Login from '../pages/Login';
+import Home from '../Home'
+export let enrutador = [
+    {
+        path: '/',
+        element: <Login />
+    },
+    {
+        path: '/home',
+        element: <Home />
+    },
+    {
+        path: '/servicios',
+        element: <h1>Servicios</h1>
+    },
+    {
+        path: '/contacto',
+        element: <h1>Contacto</h1>
+    }
+
+]
