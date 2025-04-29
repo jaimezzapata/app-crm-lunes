f52d899ae7e04ca19ad8671ed738f7eb6c5fed22
jaimezzapata <zapataval2304@gmail.com>
2025-04-28 19:28:37 -0500

    Filtrado de envios por usuario registrado

--- src/pages/ListadoEnvios.jsx
+++ src/pages/ListadoEnvios.jsx
@@ -1,7 +1,41 @@
+import { useEffect, useState } from "react";
+let urlEnvios = "http://localhost:3000/envios";
+
 const ListadoEnvios = () => {
+  let usuarioLogueado = JSON.parse(localStorage.getItem("usuario"));
+  const [envios, setEnvios] = useState([]);
+
+  function getEnvios() {
+    fetch(urlEnvios)
+      .then((response) => response.json())
+      .then((data) => setEnvios(data));
+  }
+  function filtrarEnvios() {
+    let filtradoUsuarios = envios.filter(
+      (item) => item.usuarioId == usuarioLogueado.id
+    );
+    return filtradoUsuarios;
+  }
+  let resultadoFiltrado = filtrarEnvios();
+
+  useEffect(() => {
+    getEnvios();
+  }, []);
+
   return (
-    <div className="cards">
-    </div>
+    <section className="cards">
+      {resultadoFiltrado.map((envio) => (
+        <div className="card">
+          <p>Direccion: {envio.direccion}</p>
+          <p>Fecha: {envio.fecha}</p>
+          <p>Estado: {envio.estado}</p>
+          <div className="card__buttons">
+            <button type="button">Eliminar</button>
+            <button type="button">Editar</button>
+          </div>
+        </div>
+      ))}
+    </section>
   );
 };
 
